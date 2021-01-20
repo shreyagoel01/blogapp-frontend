a9e988236c3d4ff7a493a5120b358df391a8a1e2
Shreya Goel <goelshreya34@gmail.com>
2021-01-20 22:48:54 +0530

    first commit

---
+++ blog-app/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+
+import './App.css';
+import NavBar from "./components/navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      Hello World!
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
